de4900876bf58f262818551120a136e855dfd684
min486 <smh92333@gmail.com>
2025-08-22 15:44:26 +0900

    feat: 커스텀 color palette 구성

--- app/src/main/java/desktop/hambug/ui/theme/Color.kt
+++ app/src/main/java/desktop/hambug/ui/theme/Color.kt
@@ -2,10 +2,34 @@ package desktop.hambug.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// primary palette
+val HambugRed = Color(0xFFFF6363)
+val HambugGray = Color(0xFF474D66)
+val HambugBackground = Color(0xFFFAFAF9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// secondary palette
+val Brown400 = Color(0xFF8C5A41)
+val Yellow400 = Color(0xFFE0D856)
+val Red400 = Color(0xFFF44141)
+val Green400 = Color(0xFF106B4D)
+
+// text color
+val Gray900 = Color(0xFF1C1917)
+val Gray800 = Color(0xFF292524)
+val Gray600 = Color(0xFF57534E)
+
+// border color
+val Gray500 = Color(0xFF78716C)
+val Gray400 = Color(0xFFA8A29E)
+val Gray300 = Color(0xFFD6D3D1)
+
+// icon color
+val IconGray800 = Color(0xFF292524)
+val IconGray600 = Color(0xFF78716C)
+
+// background color
+val Gray200 = Color(0xFFE7E5E4)
+val Gray100 = Color(0xFFF5F5F4)
+val Gray75 = Color(0xFFF8F8F7)
+val Gray50 = Color(0xFFFAFAF9)
+val Gray0 = Color(0xFFFFFFFF)
