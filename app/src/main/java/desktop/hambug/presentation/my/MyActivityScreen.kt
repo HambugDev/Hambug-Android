e21c2c86017dc9a57ad57949f5de338cd1800c37
min486 <smh92333@gmail.com>
2025-10-03 12:11:04 +0900

    feat: MyActivityScreen 상단 탭 구현 (게시물 / 댓글)

--- app/src/main/java/desktop/hambug/presentation/my/MyActivityScreen.kt
+++ app/src/main/java/desktop/hambug/presentation/my/MyActivityScreen.kt
@@ -16,10 +16,19 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,97 +49,127 @@ fun MyActivityScreen() {
         ) {
             Spacer(Modifier.height(16.dp))
 
-            Row(
-                modifier = Modifier.padding(start = 16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    modifier = Modifier
-                        .clickable {  }
-                        .padding(8.dp),
-                    imageVector = AppIcons.BackDetail,
-                    contentDescription = null,
-                    tint = HambugTheme.colors.iconDefault
+            MyActivityHeaderSection(
+                onClick = {}
+            )
+
+            Spacer(Modifier.height(8.dp))
+
+            // 탭 영역 (게시물 / 댓글)
+            TwoTabSection()
+        }
+    }
+}
+
+@Composable
+fun MyActivityHeaderSection(
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.padding(start = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier
+                .clickable(onClick = onClick)
+                .padding(8.dp),
+            imageVector = AppIcons.BackDetail,
+            contentDescription = null,
+            tint = HambugTheme.colors.iconDefault
+        )
+        Spacer(Modifier.width(8.dp))
+        Text(
+            text = "활동내역",
+            style = HambugTheme.typography.title02,
+            color = HambugTheme.colors.textHeadline
+        )
+    }
+}
+
+@Composable
+fun TwoTabSection() {
+    val tabs = listOf("게시물", "댓글")
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TabRow(
+            selectedTabIndex = selectedTabIndex,
+            containerColor = HambugTheme.colors.bgNormal,
+            indicator = { tabPositions ->
+                // 커스텀 인디케이터 (활성화된 탭의 아래 선)
+                TabRowDefaults.SecondaryIndicator(
+                    // 현재 선택된 탭 위치로 인디케이터를 이동시킴
+                    modifier = Modifier.tabIndicatorOffset(currentTabPosition = tabPositions[selectedTabIndex]),
+                    color = HambugTheme.colors.primRed,
+                    height = 1.5.dp
                 )
-
-                Spacer(Modifier.width(8.dp))
-
-                Text(
-                    text = "활동내역",
-                    style = HambugTheme.typography.title02,
-                    color = HambugTheme.colors.textHeadline
+            },
+            divider = {
+                HorizontalDivider(thickness = 1.5.dp, color = HambugTheme.colors.borderDefault)
+            }
+        ) {
+            tabs.forEachIndexed { idx, title ->
+                Tab(
+                    selected = selectedTabIndex == idx,
+                    onClick = { selectedTabIndex = idx },
+                    text = {
+                        val color = if (selectedTabIndex == idx) HambugTheme.colors.primRed else HambugTheme.colors.textDisabled
+                        Text(
+                            text = title,
+                            style = HambugTheme.typography.body02,
+                            color = color
+                        )
+                    },
+                    selectedContentColor = Color.White
                 )
             }
+        }
 
-            Spacer(Modifier.height(20.dp))
+        Spacer(Modifier.height(12.dp))
 
-            Column(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    Text(
-                        text = "게시물",
-                        style = HambugTheme.typography.body02,
-                        color = HambugTheme.colors.textDisabled
-//                        color = HambugTheme.colors.primRed
-                    )
-                    Text(
-                        text = "댓글",
-                        style = HambugTheme.typography.body02,
-//                        color = HambugTheme.colors.textDisabled
-                        color = HambugTheme.colors.primRed
-
-                    )
-                }
-
-                Spacer(Modifier.height(12.dp))
-
-                HorizontalDivider(thickness = 1.5.dp, color = HambugTheme.colors.borderDefault )
-            }
-
-            Spacer(Modifier.height(12.dp))
-
-            // 게시물 UI
-//            Box(
-//                modifier = Modifier
-//                    .padding(horizontal = 20.dp)
-//                    .fillMaxSize()
-//            ) {
-//                Column (
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .background(color = HambugTheme.colors.bgWhite, shape = RoundedCornerShape(6.dp))
-//                        .padding(horizontal = 20.dp)
-//                ) {
-//                    Spacer(modifier = Modifier.height(20.dp))
-//
-//                    for (i in 0 until 10) {
-//                        PostListItem()
-//                        Spacer(modifier = Modifier.height(24.dp))
-//                    }
-//                }
-//            }
-
-            // 댓글 UI
-            Box(
-                modifier = Modifier
-                    .padding(horizontal = 20.dp)
-                    .fillMaxSize()
-            ) {
-                Column (
+        // 탭 콘텐츠 (개시물 / 댓글)
+        when (selectedTabIndex) {
+            0 -> {
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = HambugTheme.colors.bgWhite, shape = RoundedCornerShape(6.dp))
                         .padding(horizontal = 20.dp)
+                        .fillMaxSize()
                 ) {
-                    Spacer(modifier = Modifier.height(20.dp))
+                    Column (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = HambugTheme.colors.bgWhite, shape = RoundedCornerShape(6.dp))
+                            .padding(horizontal = 20.dp)
+                    ) {
+                        Spacer(modifier = Modifier.height(20.dp))
 
-                    for (i in 0 until 10) {
-                        MyActivityCommentItem()
-                        Spacer(modifier = Modifier.height(24.dp))
+                        for (i in 0 until 10) {
+                            PostListItem()
+                            Spacer(modifier = Modifier.height(24.dp))
+                        }
+                    }
+                }
+            }
+            1 -> {
+                Box(
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp)
+                        .fillMaxSize()
+                ) {
+                    Column (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = HambugTheme.colors.bgWhite, shape = RoundedCornerShape(6.dp))
+                            .padding(horizontal = 20.dp)
+                    ) {
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        for (i in 0 until 10) {
+                            MyActivityCommentItem()
+                            Spacer(modifier = Modifier.height(24.dp))
+                        }
                     }
                 }
             }
